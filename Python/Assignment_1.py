64d2cfc35fab8587ab668b290351754c1904bc43
Fasih Uddin <fasihUddin002@gmail.com>
2025-04-18 00:11:37 +0500

    Assignment-1

---
+++ Python/Assignment_1.py
@@ -0,0 +1,77 @@
+# Question - 1
+a = 15 
+b = 4
+'''print("Sum :",a+b)
+print("Difference :",a-b)
+print("Product :",a*b)
+print("Division :",a/b)
+print("Floor Division :",a//b)
+print("Modulus :",a%b)'''
+
+# Question - 2
+num1 = 10 
+num2= 5.6  
+num3 = 4.43
+'''print("num1 : " ,float(num1))
+print("num2 : " ,float(num2))
+print("num3 : " ,float(num3))
+print("num1 : " ,int(num1))
+print("num2 : " ,int(num2))
+print("num3 : " ,int(num3))
+print("num1 : " ,str(num1))
+print("num1 : " ,str(num2))
+print("num1 : " ,str(num3))'''
+
+# Question - 3
+friuts = ["apple","banana","cherry","coconut","pineapple","mango","orange"]
+#print("The first friut",friuts[0])
+#print("The last friut",friuts[-1])
+#print("The fourth friut",friuts[3])
+
+# Question - 4
+Numbers = [10,20,30,40,50]
+#print("First three elements",Numbers[:3])
+#print("Last two elements",Numbers[-2:])
+#print("Except the first one",Numbers[1:])
+#print("Except the last one",Numbers[:-1])
+#print("List in reverse",Numbers[::-1])
+#print("Every second element",Numbers[::2])
+
+# Question - 5
+nums = [1,2,3]
+nums.append(4)
+#print(nums)
+
+# Question - 6
+nums = [1,2,3]
+nums.insert(1,99)
+#print(nums)
+
+# Question - 7
+nums = [1,2,3]
+nums2 = [4,5]
+nums.extend(nums2)
+#print(nums)
+
+# Question - 8
+nums = [4,1,3,2]
+nums.sort()
+#print(nums)
+
+# Question - 9
+order = ["burger","fries","coke"]
+order.append("ice cream")
+#print(order)
+order.append("biryani")
+#print(order)
+order.remove("biryani")
+#print(order) #Final Order
+
+# Question - 10
+Passengers = ["Ali","Zara","Ahmed"]
+Passengers.insert(1,"Sara")
+print(Passengers)
+Passengers.remove("Zara")
+print(Passengers)
+Passengers.reverse()
+print(Passengers)
